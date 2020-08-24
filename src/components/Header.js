3f0e91a440f0baba6cd811e6e5a85552bd0cb28a
sedx876 <swatkins876@gmail.com>
2020-08-24 11:47:38 -0400

    header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,32 @@
 import React, { Component } from 'react'
 import '../styles/Header.css'
+import { Avatar } from '@material-ui/core'
+import AccessTimeIcon from '@material-ui/icons/AccessTime'
+import SearchIcon from '@material-ui/icons/Search'
+import HelpOutlineIcon from "@material-ui/icons/HelpOutline"
 
 export default class Header extends Component {
   render() {
     return (
       <div className="header">
-        <h1>I am a Header</h1>
+        <div className="header__left">
+          <Avatar 
+            className="header__avatar"
+            alt="Sharon"
+            src="me"
+          />
+          <AccessTimeIcon />
+
+        </div>
+        <div className="header__search">
+          <SearchIcon />
+          <input placeholder="PlaceholderSearch"></input>
+
+        </div>
+        <div className="header__right">
+        <HelpOutlineIcon />
+
+        </div>
       </div>
     )
   }
