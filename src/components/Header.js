9fa718d1e7e42b586141e84bf73a010aa1a387c5
sedx876 <swatkins876@gmail.com>
2020-08-24 10:43:08 -0400

    initial commit from machine

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import '../styles/Header.css'
+
+export default class Header extends Component {
+  render() {
+    return (
+      <div className="header">
+        <h1>I am a Header</h1>
+      </div>
+    )
+  }
+}
