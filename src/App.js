bfc4cfb21c1cab179f48f16d454dec35a01b55f9
sedx876 <swatkins876@gmail.com>
2020-08-25 08:53:26 -0400

    sidebar component and sidebar css setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
-import './App.css';
+import './styles/App.css';
 import Header from './components/Header'
+import Sidebar from './components/Sidebar'
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <div className="app__body">
+        <Sidebar />
+      </div>
     </div>
   );
 }
