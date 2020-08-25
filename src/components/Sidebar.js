63df1545b1a23b6591caf5da6e1bc5fb22b05fec
sedx876 <swatkins876@gmail.com>
2020-08-25 09:24:29 -0400

    sidebar work

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,10 +1,21 @@
 import React from 'react'
 import '../styles/Sidebar.css'
+import FiberManualRecordIcon from '@material-ui/icons/FiberManualRecord'
+import CreateIcon from '@material-ui/icons/Create';
 
 function Sidebar() {
   return (
     <div className="sidebar">
-      
+      <div className="sidebar__header">
+      <div className="sidebar__info">
+      <h2>Sharon Watkins</h2>
+        <h3>
+          <FiberManualRecordIcon />
+          Mike
+        </h3>
+      </div>
+      <CreateIcon />
+      </div>
     </div>
   )
 }
