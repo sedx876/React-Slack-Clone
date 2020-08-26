15395a0febfd92ad91c44db2a93ac33ca66f3c6e
sedx876 <swatkins876@gmail.com>
2020-08-26 11:04:36 -0400

    setup firebase db

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,11 +1,37 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import '../styles/Sidebar.css'
 import FiberManualRecordIcon from '@material-ui/icons/FiberManualRecord'
 import CreateIcon from '@material-ui/icons/Create';
 import SidebarOption from './SidebarOption'
 import InsertCommentIcon from '@material-ui/icons/InsertComment';
+import InboxIcon from '@material-ui/icons/Inbox';
+import DraftsIcon from '@material-ui/icons/Drafts';
+import BookmarkBorderIcon from '@material-ui/icons/BookmarkBorder';
+import PeopleAltIcon from '@material-ui/icons/PeopleAlt';
+import AppsIcon from '@material-ui/icons/Apps';
+import FileCopyIcon from '@material-ui/icons/FileCopy';
+import ExpandLessIcon from '@material-ui/icons/ExpandLess';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import AddIcon from '@material-ui/icons/Add';
+import db from '../firebase';
 
 function Sidebar() {
+
+  const [channels, setChannels] = useState([])
+
+  useEffect(() => {
+    db.collection('rooms').onSnapshot(snapshot => (
+      setChannels(
+        snapshot.docs.map(doc => 
+          ({
+            id: doc.id,
+            name: doc.data().name
+          })
+        )
+      )
+    ))
+}, [])
+
   return (
     <div className="sidebar">
       <div className="sidebar__header">
@@ -18,7 +44,21 @@ function Sidebar() {
       </div>
       <CreateIcon />
       </div>
-      <SidebarOption Icon={InsertCommentIcon} title="Threads" />
+      {/* <SidebarOption Icon={InsertCommentIcon} title="Threads" />
+      <SidebarOption Icon={InboxIcon} title="Mentions" />
+      <SidebarOption Icon={DraftsIcon} title="Saved Items" />
+      <SidebarOption Icon={BookmarkBorderIcon} title="Channel Browser" />
+      <SidebarOption Icon={PeopleAltIcon} title="People" />
+      <SidebarOption Icon={AppsIcon} title="Apps" />
+      <SidebarOption Icon={FileCopyIcon} title="File Browser" />
+      <SidebarOption Icon={ExpandLessIcon} title="Show Less" /> */}
+      <SidebarOption Icon={ExpandMoreIcon} title="Channels" />
+      <hr />
+      <SidebarOption Icon={AddIcon} title="Add Channel" addChannelOption/>
+
+      {channels.map(channel => (
+        <SidebarOption title={channel.name} id={channel.id} key={channel.id}/>
+      ))}
     </div>
   )
 }
--- src/components/SidebarOption.js
+++ src/components/SidebarOption.js
@@ -6,7 +6,9 @@ function SidebarOption({ Icon, title }) {
     <div className="sidebarOption">
       {Icon && <Icon className="sidebarOption__icon" />}
       {Icon ? <h3>{title}</h3> : 
-      <h3><span classname="sidebarOption__hash">#</span> {title}</h3>}
+      <h3 className="sidebarOption__channel">
+      <span classname="sidebarOption__hash">#</span> {title}
+      </h3>}
     </div>
   )
 }
