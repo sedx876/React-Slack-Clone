5568d7cb8273a712812c14b7647ebdcf5b4e44d2
sedx876 <swatkins876@gmail.com>
2020-08-27 08:50:06 -0400

    chat.js and chat.css

--- src/App.js
+++ src/App.js
@@ -2,15 +2,27 @@ import React from 'react';
 import './styles/App.css';
 import Header from './components/Header'
 import Sidebar from './components/Sidebar'
+import Chat from './components/Chat'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
+    <Router>
       <Header />
       <div className="app__body">
         <Sidebar />
+        <Switch>
+
+        </Switch>
+        <Route path="/room/:roomId">
+          <Chat />
+        </Route>
+        <Route path="/">
+          <h1>Welcome</h1>
+        </Route>
       </div>
+      </Router>
     </div>
   );
 }
