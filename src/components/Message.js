573552e1f4c22fb564a05cd8b1e5524da2584e4a
sedx876 <swatkins876@gmail.com>
2020-08-28 08:45:27 -0400

    message.js component and message.css

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,11 +1,21 @@
 import React from 'react'
+import '../styles/Message.css'
 
-function Message() {
-  return (
-    <div>
-      
-    </div>
-  )
+function Message({ message, timestamp, user, userImage }) {
+    return (
+        <div className="message">
+            <img src={userImage} alt=""/>
+            <div className="message__info">
+                <h4>
+                    {user} 
+                    <span className="message__timestamp"> {new Date(timestamp?.toDate()).toUTCString()} </span>
+                </h4>
+                <p>
+                    {message}
+                </p>
+            </div>
+        </div>
+    )
 }
 
 export default Message
